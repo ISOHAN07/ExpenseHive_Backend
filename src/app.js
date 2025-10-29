05ebd16beb1aeef674c1fd8ef2e6969087ce6596
Sohan Ghosh <isohan2007@gmail.com>
2025-10-29 12:05:43 +0530

    Added Morgan

--- src/app.js
+++ src/app.js
@@ -1,14 +1,31 @@
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan")
 const expenseRoutes = require("../routes/expenses/expenses.router");
 const categoryRoutes = require("../routes/category/category.router")
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://expense-hive.vercel.app",
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps, curl)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // optional: only needed if you send cookies or auth headers
   })
 );
+
+app.use(morgan("combined"));
 
 app.use(express.json());
 app.use("/expenses", expenseRoutes);
